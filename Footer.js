e57a68022e0792e64b4535a841a464f2f2ee2f2f
cognitocapital <mikeheronis@gmail.com>
2025-01-26 07:09:01 +1000

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <p>Copyright &copy; 2023 Cognito Capital. All rights reserved.</p>
+                <div className="social-links">
+                    <a href="[invalid URL removed]" target="_blank" rel="noopener noreferrer"><img src="assets/placeholder-twitter-icon.png" alt="Twitter Icon" /></a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
